07977714662c33084e22c6a20b25b677fcd23c98
Jeremy Cui <tsuijy@gmail.com>
2011-12-19 22:50:16 +0800

    Finished the first iteration for rails example => Show the products

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '524e50a2c8343bf3f147768c71f4cea779b4d47945b1a41afb4330238c06d253b8da02eaa85d713725b6610ede9b9df5b11d83d4505fe28d063e273c4677bcd1'
