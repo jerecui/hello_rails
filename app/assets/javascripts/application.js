fa764dfae15315b86fce2a363ba8e1a7e7f272da
Jeremy Cui <tsuijy@gmail.com>
2011-12-28 21:57:41 +0800

    use Ajax to refresh the page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,19 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+jQuery.ajaxSetup({
+	'beforeSend': function(xhr) {
+		xhr.setRequestHeader("Accept", "text/javascript")
+	}
+});
+
+jQuery.fn.submitWithAjax = function() {
+	this.submit(function(){
+		$.post($(this).attr("action"), $(this).serialize(), null, "script");
+	});
+};
+
+$(function(){
+	$("#add_to_cart").submitWithAjax();
+});
--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -48,11 +48,12 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart)}
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        #format.html { redirect_to(store_url)}
+		format.js
+		#format.xml { render :xml=>@line_item, :status => :created, :location=>@line_item}
       else
         format.html { render action: "new" }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.xml { render xml: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
